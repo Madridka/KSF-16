6aea221adfdf2dc3055ba456aa584da76d946f81
Madridka <serbin_kirill1807@mail.ru>
2025-03-14 20:34:14 +0700

    add button hello Helloworld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+  <div class="app">
+    <div id="app" class="app">
+      <form  class="container">
+        <p>Текущее состояние</p>
+        <input type="text" v-model="title" placeholder="Название товара">
+        <input type="number" v-model="price" placeholder="Стоимость">
+        <input type="number" v-model="count" placeholder="Количество">
+        
+        <button @click="addProduct">Добавить товар</button>
+        <button @click="fullReset">Сбросить покупки</button>
+        <button>HELLO</button>
+      </form>
+
+      
+
+      <ol>
+        <li v-for="product in order" :key="product.id">
+          <div v-if="editingId !== product.id">
+            {{ product.title }}
+            <strong>Стоимость: </strong> {{ product.price }},
+            <strong>Количество: </strong> {{ product.count }}
+            <button @click="startEditing(product.id)">Редактировать</button>
+            <button @click="deleteProd(product.id)">Удалить</button>
+          </div>
+          <div v-else>
+            <input type="text" v-model="editTitle" placeholder="Название">
+            <input type="number" v-model="editPrice" placeholder="Стоимость">
+            <input type="number" v-model="editCount" placeholder="Количество">
+            <button @click="saveEdit">Сохранить</button>
+            <button @click="cancelEdit">Отмена</button>
+          </div>
+        </li>
+      </ol>
+
+      <div v-if="totalPrice > 0">
+        Стоимость покупки: <strong> {{ totalPrice }} рублей </strong>
+      </div>
+
+    </div>
   </div>
+
 </template>
 
 <script>
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      title: '',
+      price: '',
+      count: '', 
+      
+      order: [], 
+
+      editingId: null,
+      editTitle: '',
+      editPrice: '',
+      editCount: ''
+    }
+  },
+
+  methods: {
+    addProduct(e) {
+      e.preventDefault();
+      this.order.push({
+        id: Date.now(),
+        title: this.title,
+        price: Number(this.price),
+        count: Number(this.count),
+      })
+
+      this.title = '',
+      this.price = '',
+      this.count = ''
+    },
+
+    reset() {
+      // e.preventDefault();
+      this.title = '',
+      this.price = '',
+      this.count = ''
+    },
+
+    startEditing(id) {
+      const product = this.order.find(p => p.id === id);
+      this.editingId = id;
+      this.editTitle = product.title;
+      this.editPrice = product.price;
+      this.editCount = product.count;
+    },
+
+    saveEdit() {
+      const index = this.order.findIndex(p => p.id === this.editingId);
+      this.order[index] = {
+        id: this.editingId,
+        title: this.editTitle,
+        price: Number(this.editPrice),
+        count: Number(this.editCount)
+      };
+      this.cancelEdit();
+    },
+
+    cancelEdit() {
+      this.editingId = null;
+      this.editTitle = '';
+      this.editPrice = '';
+      this.editCount = '';
+    }
+    
+  },
+
+  computed: {
+    totalPrice() {
+      let totalPrice = 0;
+      for (const product of this.order) {
+        totalPrice += product.price * product.count;
+      }
+      return totalPrice;
+    }
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+
+.app {
+  padding: 15px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
