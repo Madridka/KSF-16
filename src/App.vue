3e695a0862795780d44f48ef295889a8e239af31
Madridka <serbin_kirill1807@mail.ru>
2025-03-22 22:26:11 +0700

    refactor: rename HelloWorld to onlineShop style: some changes in btn-styles.scss refactor: move input editPrice ref

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <onlineShop/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import onlineShop from './components/onlineShop.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    onlineShop
   }
 }
 </script>
--- src/components/onlineShop.vue
+++ src/components/onlineShop.vue
@@ -6,7 +6,9 @@
         <input type="text" v-model="title" placeholder="Название товара" />
         <input type="number" v-model="count" placeholder="Количество, шт" />
         <input type="number" v-model="price" placeholder="Стоимость, руб" />
-        <button :disabled="!isDisable" class="btn btn-add">Добавить товар</button>
+        <button :disabled="!isDisable" class="btn btn-add">
+          Добавить товар
+        </button>
       </form>
 
       <ol>
@@ -15,7 +17,7 @@
             {{ product.title }} ||
 
             <strong>Количество: </strong> {{ product.count }} шт. || ,
-            <strong>Стоимость за шт: </strong> {{ product.price }} руб.
+            <strong>Стоимость за шт.: </strong> {{ product.price }} руб.
             <button class="btn btn-edit" @click="startEditing(product.id)">
               Редактировать
             </button>
@@ -25,15 +27,15 @@
           </div>
           <div v-else>
             <input type="text" v-model="editTitle" placeholder="Название" />
-            <input type="number" v-model="editPrice" placeholder="Стоимость" />
             <input type="number" v-model="editCount" placeholder="Количество" />
+            <input type="number" v-model="editPrice" placeholder="Стоимость" />
             <button class="btn btn-save" @click="saveEdit">Сохранить</button>
             <button class="btn btn-cancel" @click="cancelEdit">Отмена</button>
           </div>
         </li>
       </ol>
 
-      <div v-if="totalPrice > 0">
+      <div class="total" v-if="totalPrice > 0">
         Стоимость покупки: <strong> {{ totalPrice }} рублей </strong>
       </div>
     </div>
@@ -41,10 +43,9 @@
 </template>
 
 <script>
-import '../assets/styles/normalize.css'
-import '../assets/styles/styles.scss'
-import '../assets/styles/btn-styles.scss'
-
+import "../assets/styles/normalize.css";
+import "../assets/styles/styles.scss";
+import "../assets/styles/btn-styles.scss";
 
 export default {
   data() {
@@ -53,7 +54,7 @@ export default {
       price: "",
       count: "",
 
-      order: [],
+      order: [{ id: 1, title: "Мопс", count: "2", price: "8000" }],
 
       editingId: null,
       editTitle: "",
@@ -96,7 +97,6 @@ export default {
     },
 
     cancelEdit() {
-
       this.editingId = null;
       this.editTitle = "";
       this.editPrice = "";
@@ -110,15 +110,18 @@ export default {
 
   computed: {
     totalPrice() {
+      // Не работает, если редактировать 
       let totalPrice = 0;
       for (const product of this.order) {
         totalPrice += product.price * product.count;
       }
       return totalPrice;
     },
+
     isDisable() {
-      return this.title !== '' && this.price !== '' && this.count !== '';
+      return this.title !== "" && this.price !== "" && this.count !== "";
     },
+
     formTitle() {
       if (this.editingId) {
         return "Редактирование товара";
@@ -131,5 +134,4 @@ export default {
 </script>
 
 <style>
-
 </style>
