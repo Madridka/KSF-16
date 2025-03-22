98fecc873f8bc2cb92a6eb9282d3e0c4fd1a4ac1
Madridka <serbin_kirill1807@mail.ru>
2025-03-22 22:53:42 +0700

    refactor: move everything in to app.vue add: added file addForm for input new product

---
+++ src/components/addForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form @submit="addNewProduct" class="container">
+      <p>{{ formTitle }}</p>
+      <input type="text" v-model="title" placeholder="Название товара" />
+      <input type="number" v-model="count" placeholder="Количество, шт" />
+      <input type="number" v-model="price" placeholder="Стоимость, руб" />
+      <button :disabled="!isDisable" class="btn btn-add">Добавить товар</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "addForm",
+  components: {},
+  data() {
+    return {};
+  },
+
+  methods: {
+    addNewProduct() {
+      this.$emit('add-product', this.addNewProduct);
+      this.addNewProduct = "";
+    }
+  },
+
+  computed: {},
+};
+</script>
+
+<style>
+</style>
